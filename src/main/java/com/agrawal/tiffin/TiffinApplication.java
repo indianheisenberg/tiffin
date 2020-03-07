417435a3f2d746988d8ec9a75c2d15f3cb7b8852
ashish <ashish.agrawal65@gmail.com>
2020-03-07 21:02:42 +0530

    my first commit

---
+++ src/main/java/com/agrawal/tiffin/TiffinApplication.java
@@ -0,0 +1,13 @@
+package com.agrawal.tiffin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiffinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiffinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agrawal/tiffin/TiffinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agrawal.tiffin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiffinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
